9ce9a255ba8979e9ce37ebc71cc6d5d1bea37285
Jaiv Patel <jaivpatel402@gmail.com>
2026-08-11 19:53:05 +0530

    add basic things

--- django+react+sql/backend/userauth/views.py
+++ django+react+sql/backend/userauth/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+
+
 
 # Create your views here.
+
+class registerView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'},status=201)   
+        
+        return Response({'error': 'Invalid data provided'}, status=400)
---
+++ django+react+sql/frontend/src/Auth/authenticate.jsx
@@ -0,0 +1,9 @@
+function authenticate() {
+    return (
+        <div>
+            <h1>Authenticate</h1>
+        </div>
+    )
+}
+
+export default authenticate
